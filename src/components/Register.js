274d4272e01ac61ea187f65e6281926d31c4dd80
Zaven Mnatzakanian <you@example.com>
2019-05-22 15:55:20 -0400

    Added register submit

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,4 +1,4 @@
-import React, {ReactDOM} from 'react';
+import React, {useCallback} from 'react';
 import {Form, Segment, Container} from 'semantic-ui-react'
 import {DIVISIONS} from '../utils/constants'
 
@@ -7,7 +7,20 @@ const divisions = DIVISIONS.map(division => ({
   text: division
 }))
 
-export default (props) => {
+const Register = (props) => {
+  const [form, setForm] = React.useState({})
+
+  const onSubmit=useCallback((e) => {
+    e.preventDefault();
+    console.log(form)
+  }, [form])
+  const onChange= useCallback((e, {name, value}) => {
+    setForm(prev => ({
+      ...prev,
+      [name]: value
+    }))
+  }, [setForm])
+
   return <Container>
     <Segment basic>
       <Segment basic>
@@ -16,20 +29,23 @@ export default (props) => {
       <Segment basic>
         <div id="registrationForm">
         </div>
-        <Form method="post" name="registration">  
-          <Form.Input required label="Team" name="team"/>
-          <Form.Input required label="Email" name="email"/>
+        <Form onSubmit={onSubmit}>  
+          <Form.Input required label="Team" name="team" value={form.team} onChange={onChange}/>
+          <Form.Input required label="Email" name="email" value={form.email} onChange={onChange}/>
           <Form.Dropdown required 
             label="Division"
             name="division"
             fluid selection
+            onChange={onChange}
+            value={form.division}
             options={divisions}
             placeholder="Choose division"/
           > 
-          <input type="hidden" name="form-name" value="registration"/>
           <Form.Button type="submit">Register</Form.Button>
         </Form>
       </Segment>
     </Segment>
   </Container>
 }
+
+export default Register;
