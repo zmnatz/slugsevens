e13938f65d85d38316de63ab08b331cf09198e26
Zaven Mnatzakanian <you@example.com>
2019-05-21 21:58:59 -0400

    added more files

---
+++ src/api/fire.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+var config = { /* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
+  apiKey: "AIzaSyBYODCCyoF-jJFYKhb8zdb2qYcwbp6w0Ns",
+  authDomain: "rockygorgetournaments.firebaseapp.com",
+  databaseURL: "https://rockygorgetournaments.firebaseio.com",
+  projectId: "rockygorgetournaments",
+  storageBucket: "rockygorgetournaments.appspot.com",
+  messagingSenderId: "802837198898"
+};
+var fire = firebase.initializeApp(config);
+export default fire;
