a51c349891f91998029c49621a052d22664ec287
zmnatz <zmnatz@penguin>
2019-06-13 16:40:42 -0400

    Added filters

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useMemo, useState } from "react";
-import { Card, Segment, Sidebar, Header } from "semantic-ui-react";
+import { Card, Segment, Sidebar, Header, Button } from "semantic-ui-react";
 
 import TeamList from "./TeamList";
 import FilterMenu from "./FilterMenu";
@@ -7,11 +7,13 @@ import Game from "./Game";
 
 import { groupBy } from "../utils/";
 import ResultContext from "../state/results";
+import useQuery from "hooks/useQuery";
 
 export default _ => {
   const { games } = React.useContext(ResultContext);
   const [sidebar, setSidebar] = useState();
   const [selected, setSelected] = useState();
+  const divisions = useQuery('divisions')
 
   const toggleSidebar = useCallback(() => setSidebar(prev => !prev), [
     setSidebar
@@ -24,9 +26,7 @@ export default _ => {
       games.filter(
         game =>
           game != null &&
-          (selected == null ||
-            game.home.id === selected ||
-            game.away.id === selected)
+          (selected == null || game.division === selected)
       ),
     [games, selected]
   );
@@ -57,23 +57,19 @@ export default _ => {
   );
 
   return useMemo(
-    () => (
+    () => <React.Fragment>
       <Segment basic>
-        <FilterMenu
-          filter={selected}
-          visible={sidebar}
-          onToggle={toggleSidebar}
-          clearSelected={clearSelected}
-        />
-        <Sidebar.Pushable as={Segment}>
-          <TeamList visible={sidebar} onSelect={setSelected} />
-
-          <Sidebar.Pusher>
-            <Segment basic style={{ minHeight: 300 }}>{scores}</Segment>
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
+        {divisions.map(division => <Button 
+          key={division}
+          positive={selected === division} 
+          onClick={()=>setSelected(division)}
+          >
+            {division}
+          </Button>
+        )}
       </Segment>
-    ),
+      <Segment basic style={{ minHeight: 300 }}>{scores}</Segment>
+    </React.Fragment>,
     [scores, sidebar, selected, clearSelected, toggleSidebar]
   );
 };
