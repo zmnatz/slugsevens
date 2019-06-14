2382b5476dc76a05628c46b0d0e5f11011486961
Zaven Mnatzakanian <zmnatz@gmail.com>
2019-06-14 09:40:07 -0400

    Displaying refs

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import {Label, List} from 'semantic-ui-react'
+import React from "react";
+import { Label, List } from "semantic-ui-react";
 
-const Score = ({score}) => 
+const Score = ({ score }) => (
   <List.Icon>
-    <Label size='large' style={{width: 40}}>
+    <Label size="large" style={{ width: 40 }}>
       {score || 0}
     </Label>
   </List.Icon>
+);
 
-export default ({game}) => {
-  const {score = {}} = game;
-  
-  return <List verticalAlign='middle'>
-    <List.Item 
-      icon={<Score score={score.away || 0}/>}
-      content={game.away.name}
-    />
-    <List.Item icon={<Score score={score.home || 0}/>
-      }
-      content={game.home.name}
-    />
-  </List>
-}
+export default ({ game }) => {
+  const { score = {} } = game;
+
+  return (
+    <List verticalAlign="middle">
+      {game.referee && game.referee.length > 0 && (
+        <h4>Official: {game.referee}</h4>
+      )}
+      <List.Item
+        icon={<Score score={score.away || 0} />}
+        content={game.away.name}
+      />
+      <List.Item
+        icon={<Score score={score.home || 0} />}
+        content={game.home.name}
+      />
+    </List>
+  );
+};
