097c06ce91a4aa0650383e5e8edee265c6168c53
Zaven Mnatzakanian <zmnatz@gmail.com>
2021-03-29 08:59:30 -0400

    updated dependencies

---
+++ src/components/TeamList.js
@@ -0,0 +1,33 @@
+import React, { useMemo } from "react";
+import { Menu, Sidebar } from "semantic-ui-react";
+import useQuery from "../hooks/useQuery";
+
+export default ({ visible, onSelect }) => {
+  const teams = useQuery("teams");
+  const sortedTeams = useMemo(() => {
+    const sorted = [...teams];
+    sorted.sort(({ name = "" }, { name: other = "" }) =>
+      name.localeCompare(other)
+    );
+    return sorted.map(team => (
+      <Menu.Item
+        key={team.id}
+        as="a"
+        content={team.name}
+        onClick={() => onSelect(team.id)}
+      />
+    ));
+  }, [teams, onSelect]);
+
+  return useMemo(
+    () => (
+      <Sidebar as={Menu} visible={visible} animation="push" vertical inverted>
+        <Menu.Item as="a" onClick={() => onSelect(null)}>
+          All Teams
+        </Menu.Item>
+        {sortedTeams}
+      </Sidebar>
+    ),
+    [sortedTeams, visible, onSelect]
+  );
+};
